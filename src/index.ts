1556a8e5e2b9a0b5459be57bcdadf17d65b6adb3
Nikita Grishin <nikita.grishin@revolut.com>
2020-08-24 10:27:09 +0100

    make workflow run

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export { default as useLoginWithPassword } from './hooks/useLoginWithPassword'
 export { default as useLoginWithGoogle } from './hooks/useLoginWithGoogle'
 export { default as useLoginWithFacebook } from './hooks/useLoginWithFacebook'
 export { default as useLogout } from './hooks/useLogout'
+
 export { default as ThauProvider } from './Provider'
